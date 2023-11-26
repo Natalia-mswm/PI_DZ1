e3e12160fecf7ecf275b58589996a2a7e7431e48
Natalia-mswm <149807177+Natalia-mswm@users.noreply.github.com>
2023-11-26 22:36:43 +0500

    TEST for streamlit.py

--- TEST for streamlit.py
+++ TEST for streamlit.py
@@ -1,24 +1,24 @@
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.efficientnet import preprocess_input
-from tensorflow.keras.applications.efficientnet import decode_predictions
-import numpy as np
+import io
+import streamlit as st
+from PIL import Image
 
-# Загружаем предварительно обученную модель EfficientNetB0
-model = EfficientNetB0(weights='imagenet')
 
-# Прописываем путь к файлу с изображением
-img_path = 'plane.jpg'
-# Загружаем изображение в память
-# EfficientNetB0 рассчитана на изображения размером 224х224
-img = image.load_img(img_path, target_size=(224, 224))
-# Выполняем предварительную обработку изображения
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-# Запускаем распознавание
-preds = model.predict(x)
-# Печатаем ТОП-3 класса с самой большой вероятностью
-classes = decode_predictions(preds, top=3)[0]
-for cl in classes:
-    print(cl[1], cl[2])
+def load_image():
+    """Создание формы для загрузки изображения"""
+    # Форма для загрузки изображения средствами Streamlit
+    uploaded_file = st.file_uploader(
+        label='Выберите изображение для распознавания')
+    if uploaded_file is not None:
+        # Получение загруженного изображения
+        image_data = uploaded_file.getvalue()
+        # Показ загруженного изображения на Web-странице средствами Streamlit
+        st.image(image_data)
+        # Возврат изображения в формате PIL
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+# Выводим заголовок страницы средствами Streamlit     
+st.title('Классификация изображений')
+# Вызываем функцию создания формы загрузки изображения
+img = load_image()
