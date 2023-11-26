b1523baa2b06d43d0b1f69a7587aba5be6fd7636
Natalia-mswm <149807177+Natalia-mswm@users.noreply.github.com>
2023-11-26 22:18:05 +0500

    PI_HW4.py

--- PI_HW4.py
+++ PI_HW4.py
@@ -1,10 +1,11 @@
 #Группа 1, Максимова Н.В.
+
 from transformers import pipeline
 import streamlit as st
 
 st.title("Перевод текста с русского на французский")
 
-text = st.text_input('Введите текст для перевода:', 'Введите текст')
-if text:
-    translator = pipeline("translation_ru_to_fr", "Helsinki-NLP/opus-mt-ru-fr")
-    text2 = st.write("Перевод: ", translator(text))
+translator = pipeline("translation_ru_to_fr", "Helsinki-NLP/opus-mt-ru-fr")
+st.text = 'Перевод для предложения: "Это совсем непросто!"'
+text = "Это совсем непросто!"
+st.write("Перевод: ", translator(text))
